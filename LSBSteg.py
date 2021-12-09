a854f9bc7ebffebf6fb3d2c5f0af7be6dc6c897e
Cyrus Cursetjee <ccursetjee@gmail.com>
2021-12-09 15:36:23 -0500

    attempting to encode message in binary image array

--- LSBSteg.py
+++ LSBSteg.py
@@ -1,8 +1,30 @@
 import bitarray
-import base64
+import numpy as np
+from PIL import Image
 
 message = 'I finally finished all my math homework.'
 
-ba = bitarray.bitarray()
-ba.frombytes(message.encode('utf-8'))
-print(ba)
+# im = Image.open('grumpycat.jpg')
+# im.save('lsb_grumpycat.jpg')
+
+def encodeImg(src, message):
+	img = Image.open(src, 'r')
+	width, height  = img.size
+	imgData = np.array(list(img.getdata()))
+	print(img.mode)
+	print(imgData)
+
+	totalPixels = imgData.size // 3
+	print(totalPixels)
+
+	ba = bitarray.bitarray()
+	ba.frombytes(message.encode('utf-8'))
+	print(len(ba))
+
+	if len(ba) > totalPixels:
+		print("Error: message too long")
+	else:
+		a = int(bin(imgData[1][1])[2:9] + int(ba[0]), 2)
+		print(a)
+
+encodeImg('grumpycat.jpg', message)
