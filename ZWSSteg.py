ce3e5945373c0ccd2bf087f8a4ea264456a9817d
Cyrus Cursetjee <ccursetjee@gmail.com>
2022-01-07 13:49:06 -0500

    fixed nmber and special character problem

--- ZWSSteg.py
+++ ZWSSteg.py
@@ -26,41 +26,47 @@ def encode(source, message):
 	f = open(source, 'r', encoding='utf-8')
 	email = f.read()
 
-	email_array = to_byte_array(email)
-	message_array = to_byte_array(message)
+	if len(email) < len(message):
+		print("Message exceeds length of email")
 
-	message_binary = []
-	for x in message_array:
-		for i in range(len(x)-1):
-			if (i >= 1):
+	else:
+		email_array = to_byte_array(email)
+		message_array = to_byte_array(message)
+
+		#print(message_array)
+		message_binary = []
+		for x in message_array:
+			if (len(x)<=8):
+				 x = "0" * (9-len(x)) + x
+			for i in range(len(x)):
+				if (x[i] != 'b'):
+					message_binary.append(x[i])	
+		#print(message_binary)
+		
+		i = 1
+		for x in message_binary:
+			if (int(x)):
+				email_array.insert(i, "1")
 				i += 1
-			message_binary.append(x[i])	
-	
-	i = 1
-	for x in message_binary:
-		if (int(x)):
-			email_array.insert(i, "1")
 			i += 1
-		i += 1
-	email_array.insert(i, "0")
-	#print(email_array)
-	
-	f = open("encoded.txt", "w", encoding="utf-8")
-	counter = 0
-	for x in email_array:
-		if (x == "0"):
-			f.write(u'\u200D\u200D')
-			#f.write('!!')
-		elif (x == "1"):
-			f.write(u'\u200D')
-			#f.write('!')
-		elif (x == '0b11100010' or x == '0b10010111' or x == '0b10001111'):
-			if (x == '0b11100010'):
-				f.write(u'\u25CF')
-		else:
-			f.write(to_char(x))
-	f.close()
-	print("Encoded successfully!")
+		email_array.insert(i, "0")
+		#print(email_array)
+		
+		f = open("encoded.txt", "w", encoding="utf-8")
+		for x in email_array:
+			if (x == "0"):
+				f.write(u'\u200D\u200D')
+				#f.write('!!')
+			elif (x == "1"):
+				f.write(u'\u200D')
+				#f.write('!')
+			elif (x == '0b11100010' or x == '0b10010111' or x == '0b10001111'):
+				if (x == '0b11100010'):
+					f.write(u'\u25CF')
+			else:
+				f.write(to_char(x))
+		f.close()
+		print("Encoded successfully into encoded.txt!")
 
 #01100001
 #zws = '0b11100010', '0b10000000', '0b10001011'
@@ -75,7 +81,7 @@ def decode(source):
 		email_array.remove("0b10000000")
 	while('0b10001101' in email_array):
 		email_array.remove("0b10001101")
-	#print(email_array)
+	# print(email_array)
 
 	message_array = []
 	previous = 0
@@ -96,20 +102,15 @@ def decode(source):
 	for x in message_array:
 		message += x
 	#print(message)
-	message = message.replace('00000000', '')
-	message = message.replace('0100000', '00100000')
 	message = message[:-1]
-	#print(message)
 
+	# print(message_array)
 	message_array = space(message)
 	#print(message_array)
 	
 
 	for x in message_array:
-		if (len(x) < 8):
-			break
 		print(to_char(x), end="")
-	print()
 
 
 if (len(sys.argv) < 3):
@@ -121,10 +122,3 @@ elif (sys.argv[1] == 'encode'):
 		encode(sys.argv[2], sys.argv[3])
 elif (sys.argv[1] == 'decode'):
 	decode(sys.argv[2])
-
-#n = len(sys.argv)
-#print("Total arguments passed:", n)
- 
-
-#email1.txt
-#message = "I found a cat yay"
