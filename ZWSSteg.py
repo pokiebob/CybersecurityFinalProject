b08f9745a1cbb1dba1b976f9037b35c4117c357b
Helen Zhu <hzhu20@stuy.edu>
2022-01-07 04:57:29 -0500

    ZWS encodes properly

--- ZWSSteg.py
+++ ZWSSteg.py
@@ -1,31 +1,50 @@
 def to_byte_array(s):
-    s_bytearray = bytearray(s, "utf8")
-    s_array = []
-    for x in s_bytearray:
-        b = bin(x)
-        s_array.append(b)
-    return s_array
+	s_bytearray = bytearray(s, "utf8")
+	s_array = []
+	for x in s_bytearray:
+		b = bin(x)
+		s_array.append(b)
+	return s_array
+
+
+def to_char(binary_string):
+	i = int(binary_string, 2)
+	c = chr(i)
+	return c
 
 
 def encode(source, message):
 	f = open(source, 'r', encoding='utf-8')
 	email = f.read()
 	email_array = to_byte_array(email)
-	
-	total_length = len(email_array)
 	message_array = to_byte_array(message)
-	message_length = len(message_array)
 
-	if (True):
-		print(email_array)
-		print(message_array)
-		print(total_length)
-		print(message_length)
+	message_binary = []
+	for x in message_array:
+		for i in range(len(x)-1):
+			if (i >= 1):
+				i += 1
+			message_binary.append(x[i])	
+	
+	i = 1
+	for x in message_binary:
+		if (int(x)):
+			email_array.insert(i, "1")
+			i += 1
+		i += 1
+	#print(email_array)
+	
+	f = open("new.txt", "w")
+	for x in email_array:
+		if (x == "1"):
+			#f.write(u'\u200b')
+			f.write('!')
+		else:
+			f.write(to_char(x))
+	f.close()
+
+
+
 
 message = "I found a cat yay"
 encode('email1.txt', message)
-
-
-
-
-#11100010 10000000 10001011	zws
